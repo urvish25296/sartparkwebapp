ccbc88a5b569d2a83d9725eb818cc07a610b8c3e
urvish <urvish25296@gmail.com>
2021-04-23 12:10:34 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, AppRegistry } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App () {
+	return <WebView source={{ uri: 'https://smart-park-frontend-9v01lom1w-urvish25296.vercel.app/' }} />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container : {
+		flex            : 1,
+		backgroundColor : '#fff',
+		alignItems      : 'center',
+		justifyContent  : 'center'
+	}
 });
